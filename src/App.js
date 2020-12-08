e495873806bce51ecf99143596b304116d25a1aa
Engr-Asad-Hussain <asad.h1998@yahoo.com>
2020-12-08 19:15:24 +0000

    Added following files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Games from './game';
+
+function App() {
+  return (
+    /* <Games></Games> */
+    <div>  
+      <Games gameName1="Dota 2" gameName2="Counter Strike Global Offensive" />
+      <hr />
+      <Games gameName1="Dota 2" gameName2="Counter Strike Global Offensive" />
+      <hr />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/game.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Games(props)  {
+    return(
+        <div>
+            <h1>Most played Games of 2020</h1>
+            <p>Following are the best games played in 2020</p>
+            <ul>
+                <li>Dota 2</li>
+                <li>Counter Strike Global Offensive</li>
+                <li>PUBG</li>
+                <li>Empire Earth</li>
+                <li>Age of Empire</li>
+            </ul>
+            <h1>User's Feedback:</h1>
+            <p>I love playing following games since past 10 years</p>
+            <ul>
+                <li>I played {props.gameName1}</li>
+                <li>I played {props.gameName2}</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Games;
